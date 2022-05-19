df355bdeaeb5b0fb8013d57039335cff06d9e738
SundayCoder81 <richard.bogu@gmail.com>
2022-05-19 22:16:13 +0300

    was testing db extension

--- database/dB_Creation.php
+++ database/dB_Creation.php
@@ -1,72 +1,27 @@
 <?php
-//PHP Connect to MySQL
+// This is procedural way of establishing connection with database, whole point of this program is to practice a bit of queries along with PHP
+
+
+// DataBase login credentials
 
 $servername = "localhost";
-$username = "moderator";
-$password = "haslo-moda";
+$username = "root";
+$password = "123";
 
 // Create connection
+
 $conn = mysqli_connect($servername, $username, $password);
 
-// Check connection
+
+// Checking for connection
+
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+echo "Conected successfully";
 
 
-// PHP Create a MySQL Database
-
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . $conn->error;
-}
-
-$conn->close();
-
-// PHP MySQL Create Table
+$sql = "CREATE DATABASE newDB";
 
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-// sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
-
-if (mysqli_query($conn, $sql)) {
-  echo "Table MyGuests created successfully";
-} else {
-  echo "Error creating table: " . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-?>

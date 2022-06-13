d347b4db7a6d7171b9c66463363707e901986b74
Elzhbietta <102871990+Elzhbietta@users.noreply.github.com>
2022-06-13 21:46:10 +0300

    test

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ include_once 'header.php';
   <!-- used "mx-auto" to align items horizontally -->
   <div class="container w-100">
     <div id="pizza-menu1" class="row mb-5">
-      <img class="col-4 d-flex justify-content-centers" src="img/pizza-bazilica.jpeg" alt="pizza basilica">
-      <img class="col-4 d-flex justify-content-centers" src="img/pizza-bazilica.jpeg" alt="pizza basilica">
-      <img class="col-4 d-flex justify-content-centers" src="img/pizza-bazilica.jpeg" alt="pizza basilica">
+      <img class="col-4 d-flex justify-content-centers" src="img/astrioji NEW.png" alt="pizza basilica">
+      <img class="col-4 d-flex justify-content-centers" src="img/grybu NEW.png" alt="pizza basilica">
+      <img class="col-4 d-flex justify-content-centers" src="img/4 sezonu NEW.png" alt="pizza basilica">
     </div>
     <div id="pizza-menu2" class="row mb-5">
-      <img class="col-4 d-flex justify-content-center mx-auto" src="img/pizza-bazilica.jpeg" alt="pizza basilica" width="20%">
-      <img class="col-4 d-flex justify-content-center mx-auto" src="img/pizza-bazilica.jpeg" alt="pizza basilica" width="20%">
-      <img class="col-4 d-flex justify-content-center mx-auto" src="img/pizza-bazilica.jpeg" alt="pizza basilica" width="20%">
+      <img class="col-4 d-flex justify-content-center mx-auto" src="img/kaimiška NEW.png" alt="pizza basilica" width="20%">
+      <img class="col-4 d-flex justify-content-center mx-auto" src="img/pepperoni NEW.png" alt="pizza basilica" width="20%">
+      <img class="col-4 d-flex justify-content-center mx-auto" src="img/meksikietiska NEW.png" alt="pizza basilica" width="20%">
     </div>
   </div>
 </div>

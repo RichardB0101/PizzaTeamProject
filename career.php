95e734c87ed5e9bd13bf0499560afcfd23ba821e
Elzhbietta <102871990+Elzhbietta@users.noreply.github.com>
2022-06-14 22:52:44 +0300

    career

--- career.php
+++ career.php
@@ -6,9 +6,31 @@ include_once 'header.php';
 
         <h2 class="text-center mt-5 mb-5">Career</h2>
 
+    <div class="row mb-5">
+<img class="img-fluid" src="img/career1.jpg" alt="pizza basilica"></div>
+        
+        <div class="container w-50">
+
+
+        <p> Jei nori tobulėti, išmokti kažką naujo, išbandyti ir įvertinti save, pataikei kaip tik ten, kur ir reikia. PIZZA siekia darbui suburti  darbščių, optimistiškai nusiteikusių žmonių kolektyvą, todėl visada yra atvira aktyviems, komunikabiliems, entuziastu trykštantiems ir užsispyrusiems žmonėms, kurie nuosekliai siekia savo užsibrėžtų tikslų</p></br>
+
+<p>Kodėl gi ir Tau neprisijungus prie mūsų komandos? Jei esi darbštus, atsakingas, ambicingas, mėgsti bendrauti ir tiki, kad nuolatinis tobulėjimas yra gyvenimo variklis, PIZZA - puiki vieta tavo karjerai.</p></br>
+
+<p>Mes Tau siūlome:</p>
+
+<p>• visada laiku mokamą atlyginimą;</p>
+<p>• visas socialines garantijas;</p>
+<p>• tobulėjimo ir karjeros galimybes;</p>
+<p>• lankstų darbo grafiką, galimybę darbą derinti su studijomis;</p>
+<p>• puikias darbo sąlygas, jauną ir draugišką kolektyvą;</p>
+<p>• tik darbuotojams skirtas įmonės ir jos partnerių nuolaidas.</p>
+<p>Nedvejok ir prisijunk prie PIZZA komandos!</p>
+
+<p>Siųsk savo CV  bei motyvacinį laišką adresu PizzaTeamProject@pizza.lt.</p>
+    </div>
     </div>
 
-
+</div>
 
 
 

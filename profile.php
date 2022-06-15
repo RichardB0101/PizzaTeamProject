59dc91c87ec6d380b4c47f4051f013708e995cac
SundayCoder81 <richard.bogu@gmail.com>
2022-06-15 14:00:43 +0300

    small fix

--- profile.php
+++ profile.php
@@ -2,14 +2,9 @@
 session_start();
 
 if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
-
+include_once 'header.php';
  ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>HOME</title>
-</head>
-<body>
+
      <h1>Hello, <?php echo $_SESSION['username']; ?></h1>
      <a href="logout.php">Logout</a>
 </body>

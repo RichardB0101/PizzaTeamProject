7f1c327e3c3a5463074092b8f188a6e0e43003e6
SundayCoder81 <richard.bogu@gmail.com>
2022-06-30 01:53:26 +0300

    added small update

--- admin.php
+++ admin.php
@@ -1,22 +1,10 @@
-    <?php
-    require_once("database/dB_Connection.php");
+<?php
+include_once 'header.php';
+require_once 'database/dB_Connection.php';
+?>
 
-    ?>
-
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <title>Document</title>
-    </head>
-
-    <body>
-        <div class="w-100">
+<h2 class="mt-3 text-center">Admin control panel</h2>
+        <div class="mt-5 w-100">
             <table class="table text-light bg-dark">
                 <tr>
                     <th>User_ID</th>
